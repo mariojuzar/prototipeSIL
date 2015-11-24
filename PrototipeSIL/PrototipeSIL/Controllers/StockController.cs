9abd9b63a9881dd0614de543d8faba93fa06eb26
Mario Tressa Juzar <mariotj.tj@gmail.com>
2015-11-24 20:58:00 +0700

    show stok and supplier

--- PrototipeSIL/PrototipeSIL/Controllers/StockController.cs
+++ PrototipeSIL/PrototipeSIL/Controllers/StockController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PrototipeSIL.DAL;
 
 namespace PrototipeSIL.Controllers
 {
     public class StockController : Controller
     {
+        SILDBEntities db = new SILDBEntities();
+
         // GET: Stock
         public ActionResult Index()
         {
-            return View();
+            List<Stock> stok = db.Stock.ToList();
+
+            return View(stok);
         }
     }
 }
--- PrototipeSIL/PrototipeSIL/Controllers/SupplierController.cs
+++ PrototipeSIL/PrototipeSIL/Controllers/SupplierController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PrototipeSIL.DAL;
 
 namespace PrototipeSIL.Controllers
 {
     public class SupplierController : Controller
     {
+        SILDBEntities db = new SILDBEntities();
+
         // GET: Supplier
         public ActionResult Index()
         {
-            return View();
+            List<Supplier> supplier = db.Supplier.ToList();
+
+            return View(supplier);
         }
     }
 }
